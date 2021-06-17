79f63d85e7a57b2835e46e6ae6259aa5f8358fad
cjmurphy93 <cjmurph93@gmail.com>
2021-06-16 22:26:24 -0400

    added onPressed to roundiconbutton

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -180,9 +180,11 @@ class _InputPageState extends State<InputPage> {
 class RoundIconButton extends StatelessWidget {
   RoundIconButton({
     this.icon,
+    this.onPressed,
   });
 
   final IconData icon;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -190,7 +192,7 @@ class RoundIconButton extends StatelessWidget {
       child: Icon(
         icon,
       ),
-      onPressed: () {},
+      onPressed: onPressed,
       elevation: 6.0,
       constraints: BoxConstraints.tightFor(
         width: 56.0,
