ed1e18663af7e2cb9e07c07af8b5bf74fcc722bf
cjmurphy93 <cjmurph93@gmail.com>
2021-06-21 21:42:25 -0400

    ran a clean build of android

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'BMI CALCULATOR',
-          ),
+      appBar: AppBar(
+        title: Text(
+          'BMI CALCULATOR',
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                  child: Text(
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text(
                 'Your Result',
-                    style: kTitleTextStyle,
-              )),
+                style: kTitleTextStyle,
+              ),
             ),
-          ],
-        ));
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
