d65e28ee2dc9c5b594a8b7cfb0303afc904fe3ab
cjmurphy93 <cjmurph93@gmail.com>
2021-06-21 21:20:26 -0400

    started styling results page

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'BMI CALCULATOR',
+        appBar: AppBar(
+          title: Text(
+            'BMI CALCULATOR',
+          ),
         ),
-      ),
-      body: Text(
-        'Hello',
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                  child: Text(
+                'Your Result',
+                    style: kTitleTextStyle,
+              )),
+            ),
+          ],
+        ));
   }
 }
